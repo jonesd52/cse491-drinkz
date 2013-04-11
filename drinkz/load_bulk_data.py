24db5f865ff7455dbbbec1bf5866e97c15f2944e
David Jones <jonesd52@arctic.cse.msu.edu>
2013-04-10 20:19:01 -0400

    Added bulk loading of recipes

--- drinkz/load_bulk_data.py
+++ drinkz/load_bulk_data.py
@@ -10,7 +10,7 @@ Module to load in bulk data from text files.
 
 import csv                              # Python csv package
 
-from . import db                        # import from local package
+from . import db, recipes                        # import from local package
 
 def read_data(i_file):
   
@@ -22,7 +22,13 @@ def read_data(i_file):
         if not line or not line[0].strip() or line[0].startswith('#'):
             continue
         yield line
+
+def read_recipe_data(i_file):
     
+    for line in i_file:
+        if not line or not line[0].strip() or line[0].startswith('#'):
+             continue
+        yield line
     
 
 def load_bottle_types(fp):
@@ -84,4 +90,35 @@ def load_inventory(fp):
         db.add_to_inventory(mfg, name, amount)
 
     return n
+
+
+def load_recipes(fp):
+    "Load bulk number of recipes"
+
+    new_reader = read_recipe_data(fp)
+
+    n = 0
+    
+    for line in new_reader:
+        x = []
+        line = line.split('|')
+        name = line[0]
+        ingredients = line[1]
+        ingredients = ingredients.split('/')
+        while (ingredients != []):
+             r = ingredients[0].split(',')
+	     ingredients.pop(0)
+             t = (r[0], r[1])
+	     x.append(t)
+
+        recipe = recipes.Recipe(name, x)
+        db.add_recipe(recipe)
+	n += 1
+    
+    return n
+
+
+        
+
+
 
